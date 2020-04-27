0ef33c3a64e8aa5ee6cf4f550a58c471559c60da
Tetra <seifaziz10@gmail.com>
2020-04-26 18:56:01 -0700

    "Our Doctors" Page & Navbar Fix

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function () {
-  if (window.scrollY == 0) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
-  }
-} 
