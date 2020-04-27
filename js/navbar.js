a8641427e14d9db4e102c586ef19e287d69c8c00
Tetra <seifaziz10@gmail.com>
2020-04-26 17:46:53 -0700

    Fixed navbar bug

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,7 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+window.onscroll = function () {
+  if (window.scrollY == 0) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-100px";
   }
-  prevScrollpos = currentScrollPos;
 } 
